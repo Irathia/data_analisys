11b8c69af13a052b76ce4a52264b69349cd5ac52
Irathia <erathia.irathia@gmail.cim>
2014-09-26 14:33:18 +0400

    added file with filenames and industry
    we have 30 companies  and 4 industries, such as Money, Realestate,
    Conglomerate and Other

--- main.R
+++ main.R
@@ -1,7 +1,12 @@
 source("readdata.R")
 source("correlation.R")
+source("linearregr.R")
 
-data = readdata("test_data")
+data = readdata("data")
 
-crate = correlation(data$rate,4)
-cvolume = correlation(data$value,4)
+industry = readLines("filename.txt")
+industry = strsplit(industry,split = ";")
+
+cdata = correlation(data,4)
+#cvolume = correlation(data$value,4)
+
