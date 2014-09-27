28c3c662cbee3d28a8c677876bdb189af1beaa4d
Ilya Churaev <ilya.churaev@intel.com>
2014-09-27 22:48:12 +0400

    Work version
    Signed-off-by: Ilya Churaev <ilya.churaev@intel.com>

--- main.R
+++ main.R
@@ -5,16 +5,19 @@ source("readind.R")
 source("tabreg.R")
 source("neibors.R")
 
-#data = readdata("test_data")
+data = readdata("data")
 
-crate = correlation(data, "rate")
-cvolume = correlation(data, "volume")
+#crate = correlation(data, "rate")
+#cvolume = correlation(data, "volume")
 
-rrate = regression(data, "rate")
-rvolume = regression(data, "volume")
+#rrate = regression(data, "rate")
+#rvolume = regression(data, "volume")
 
 ind <- readInd("filename.txt")
-data <- list(rate=data$rate,value=data$value,date=data$date,filename=data$filename,industry = ind)
-tabregrate(data)
-tabregvalue(data)
-neibors(data,k)
+for (i in 1:length(ind)){
+  data[[i]][["industry"]] <- ind[i]
+}
+#data <- list(rate=data$rate,value=data$value,date=data$date,filename=data$filename,industry = ind)
+#tabreg(data)
+#tabregvalue(data)
+#neibors(data,k)
